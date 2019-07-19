fcca3d00a62dae5a8b957728ffd556d2da98c462
Ruth Newman <ruthnewman@gmail.com>
2019-07-19 01:02:12 +0100

    JSX exploration challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,54 @@
 console.log('App.js is running');
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'Does this change?'
+  React.createElement(
+    'h1',
+    null,
+    'Doubt Debugger'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Make decisions quickly'
+  ),
+  React.createElement(
+    'ul',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Yes or no?'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Choose between options'
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Ruth Newman'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 30'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: London'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 console.log('App.js is running');
 
-let template = <p>Does this change?</p>;
+let template = (
+  <div>
+    <h1>Doubt Debugger</h1>
+    <p>Make decisions quickly</p>
+    <ul>
+      <li>Yes or no?</li>
+      <li>Choose between options</li>
+    </ul>
+  </div>
+);
+
+let templateTwo = (
+  <div>
+    <h1>Ruth Newman</h1>
+    <p>Age: 30</p>
+    <p>Location: London</p>
+  </div>
+);
 
 let appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
