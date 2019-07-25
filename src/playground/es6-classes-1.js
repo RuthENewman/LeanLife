97ebeef99e8359614be52a1f4c86d64045870741
Ruth Newman <ruthnewman@gmail.com>
2019-07-25 21:54:27 +0100

    added second argument for age

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,18 +1,24 @@
 class Person {
-  constructor(name = 'Anonymous') {
+  constructor(name = 'Anonymous', age = 0) {
     this.name = name;
+    this.age = age;
   }
 
   getGreeting() {
-    // return 'Hi ' + this.name + "!";
     return `Hi there, ${this.name}!`;
+  }
+
+  getDescription() {
+    return `${this.name} is ${this.age} years old.`
   }
 
 }
 
-const me = new Person('Ruth Newman');
+const me = new Person('Ruth Newman', 30);
 
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
+console.log(other.getDescription());
