36463a532d8402cbdb3ddf1064f1180f4aca85ec
Ruth Newman <ruthnewman@gmail.com>
2019-07-28 23:00:10 +0100

    exploring ES6 class properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,30 @@ import ReactDOM from 'react-dom';
 import DoubtDebuggerApp from './components/DoubtDebuggerApp';
 
 ReactDOM.render(<DoubtDebuggerApp />, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Ruth';
+  }
+
+  getGreeting() {
+    return `Hi. My name is ${this.name}.`
+  }
+}
+
+const oldSyntax = new OldSyntax();
+
+// console.log(oldSyntax.getGreeting());
+// const getGreeting = oldSyntax.getGreeting();
+// console.log(getGreeting());
+
+class NewSyntax {
+  name = 'Ellie';
+  getGreeting = () => {
+    return `Hi, my name is ${this.name}.`
+  }
+}
+
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
