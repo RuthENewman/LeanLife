1fc78575e147ff67c2996ecf89026c5537d1e9eb
Ruth Newman <ruthnewman@gmail.com>
2019-07-31 00:55:13 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DoubtDebuggerApp from './components/DoubtDebuggerApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const Dashboard = () => (
+  <div>
+    Dashboard component
+  </div>
+);
 
-ReactDOM.render(<DoubtDebuggerApp />, document.getElementById('app'));
+const AddExpense = () => (
+  <div>
+    Add an expense
+  </div>
+);
+
+const EditExpense = () => (
+  <div>
+    Edit an expense
+  </div>
+);
+
+const Help = () => (
+  <div>
+    Help
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Dashboard}/>
+      <Route path="/create" component={AddExpense}/>
+      <Route path="/edit" component={EditExpense}/>
+      <Route path="/help" component={Help}/>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<p>This is my boiler plate for Lean Life</p>, document.getElementById('app'));
