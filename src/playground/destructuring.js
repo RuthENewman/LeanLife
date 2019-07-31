f1be56c038bee26cba103804ce114f7886a08a3c
Ruth Newman <ruthnewman@gmail.com>
2019-07-31 22:35:24 +0100

    default and renaming using destructuring syntax

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,23 +1,29 @@
-console.log('destructuring');
-
-const person = {
-  name: 'Ruth',
-  age: 30,
-  location: {
-    city: 'London',
-    temp: 36
-  }
-};
-
-const { name, age } = person;
-
-console.log(`${name} is ${age}.`);
-
-// if (person.location.city && person.location.temp) {
-//   console.log(`It\'s ${person.location.temp} C in ${person.location.city}.`);
+ // const person = {
+//   name: 'Ruth',
+//   age: 30,
+//   location: {
+//     city: 'London',
+//     temp: 36
+//   }
+// };
+//
+// const { name: firstName = 'Stranger', age } = person;
+//
+// console.log(`${firstName} is ${age}.`);
+//
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It\'s ${temperature} C in ${city}.`);
 // }
 
-const { city, temp } = person.location;
-if (city && temp) {
-  console.log(`It\'s ${temp} C in ${city}.`);
+const book = {
+  title: 'Hide and Seek',
+  author: 'Ian Rankin',
+  publisher: {
+    name: 'Orion'
+  }
 }
+
+const { name: publisherName = 'Self published'} = book.publisher;
+
+console.log(publisherName);
