90ad7eb3e67675667ca69ec4a4cbca2251638666
Ruth Newman <ruthnewman@gmail.com>
2019-07-31 22:51:03 +0100

    destructuring with arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// OBJECT DESTRUCTURING
+
  // const person = {
 //   name: 'Ruth',
 //   age: 30,
@@ -16,14 +18,31 @@
 //   console.log(`It\'s ${temperature} C in ${city}.`);
 // }
 
-const book = {
-  title: 'Hide and Seek',
-  author: 'Ian Rankin',
-  publisher: {
-    name: 'Orion'
-  }
-}
+// const book = {
+//   title: 'Hide and Seek',
+//   author: 'Ian Rankin',
+//   publisher: {
+//     name: 'Orion'
+//   }
+// }
+//
+// const { name: publisherName = 'Self published'} = book.publisher;
+//
+// console.log(publisherName);
 
-const { name: publisherName = 'Self published'} = book.publisher;
 
-console.log(publisherName);
+// ARRAY DESTRUCTURING
+
+const address = ['4 Crescent Court', 'London', 'United Kingdom', 'N21 8JP'];
+
+const [street, city, country, postCode] = address;
+
+// const [, , country = 'London'] = address;
+
+console.log(`You are at ${street} in ${city}.`);
+
+const item = ['Speculoos ice cream', '£3.50', '£4.50', '£5.50']
+
+const [flavour, oneScoop, twoScoops, threeScoops] = item;
+
+console.log(`Two scoops of ${flavour} costs ${twoScoops}.`);
