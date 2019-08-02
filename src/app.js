de3cc37e6315c54d97943ab0a45b36acea73c613
Ruth Newman <ruthnewman@gmail.com>
2019-08-02 23:52:49 +0100

    refactored redux into seperate files and directories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import AppRouter from './routers/AppRouter';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Council tax', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas and lekkie bill', amount: 9300 }));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
