9e659ee5c2a7cc47cee85662eae55d3a5df0886c
Ruth Newman <ruthnewman@gmail.com>
2019-08-04 01:05:02 +0100

    setting up a date picker

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ store.dispatch(addExpense({ description: 'Monthly rent', amount: 70000 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
