69899802a164d591f748e3b999bb90f479957673
Ruth Newman <ruthnewman@gmail.com>
2019-08-06 00:23:21 +0100

    debugged issues and now working fully

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { editExpense, removeExpense } from
+'../actions/expenses';
 
 const EditExpense = (props) => {
   return (
@@ -9,21 +10,24 @@ const EditExpense = (props) => {
       <ExpenseForm
         expense={props.expense}
         onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
+          props.dispatch(editExpense(props.expense.id,
+            expense));
           props.history.push('/');
         }}
       />
       <button onClick={() => {
-        dispatch(removeExpense({ id: props.expense.id }));
+        props.dispatch(removeExpense({ id:
+          props.expense.id }));
         props.history.push('/');
       }}>Remove</button>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, props) => {
   return {
-    expense: state.expenses.find((expense) =>  expense.id === props.match.params.id;
+    expense: state.expenses.find((expense) =>  expense.id
+    === props.match.params.id)
   };
 };
 
