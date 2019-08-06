32f0f347f2f23221e307f08747c1259ffa9a08c6
Ruth Newman <ruthnewman@gmail.com>
2019-08-07 00:38:48 +0100

    snapshot testing with dynamic components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('renders Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
-  // expect(wrapper.find('h1').text()).toBe('Lean Life');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
