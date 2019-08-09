1c2204d72b9cd41f9e66fbc34e12614275f0e4c8
Ruth Newman <ruthnewman@gmail.com>
2019-08-09 19:07:24 +0100

    Set up formatting for amount and created at

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -95,6 +95,7 @@ export default class ExpenseForm extends React.Component {
           onFocusChange={this.onFocusChange}
           numberOfMonths={1}
           isOutsideRange={() => false}
+          displayFormat={() => "DD/MM/Y"}
           />
           <textarea
           placeholder="Add a note for your expense"
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+import '../locales/sterling.js';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
+const ExpenseListItem = ({ id, description, amount, createdAt }) => {
+  numeral.locale('en-gb');
+  return (<div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      <span>{numeral(amount / 100).format('$#,##0.00')} </span>
+       -
+      <span> {moment(createdAt).format("dddd Do MMMM YYYY")}</span>
+    </p>
   </div>
 );
+}
 
 export default ExpenseListItem;
