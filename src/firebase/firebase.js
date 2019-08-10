79247c4048dbdc5bf931fd2a2d5b0b6744106b01
Ruth Newman <ruthnewman@gmail.com>
2019-08-10 22:37:04 +0100

    reading and writing to the database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,31 +14,92 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Ruth Newman',
-  age: 30,
-  stressLevel: 9,
-  job: {
-    title: 'Software Developer',
-    company: 'Google'
-  },
-  location: {
-    city: 'London',
-    country: 'United Kingdom'
-  }
-}).then(() => {
-  console.log('Data saved to the database');
-}).catch((error) => {
-  console.log('Failed to save correctly: ', error);
+database.ref('expenses').push({
+  description: 'Groceries',
+  amount: 1245,
+  note: '',
+  createdAt: 43785234325984
 });
+
+database.ref('expenses').push({
+  description: 'Rent',
+  amount: 70000,
+  note: '',
+  createdAt: 23464975398
+});
+
+database.ref('expenses').push({
+  description: 'Zone 3 Travelcard',
+  amount: 3599,
+  note: '',
+  createdAt: 43785234325984
+});
+
+// database.ref('notes/-LlxPn8KvVgMdaVxulVX').remove();
+
+// database.ref('notes').push({
+//   title: 'Course topics',
+//   body: 'GraphQL, PHP, Laravel, Node.JS'
+// });
+
+// database.ref().on('value', (snapshot) => {
+//   const value = snapshot.val();
+//   console.log(`${value.name} is a ${value.job.title} at ${value.job.company}.`);
+// })
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (error) => {
+//   console.log('Error when fetching data', error);
+// });
+//
+// setTimeout(() => {
+//   database.ref('age').set(31);
+// }, 3000);
+//
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange);
+// }, 6000);
+//
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 9000);
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((error) => {
+//     console.log('Oops, something went wrong when fetching data', error);
+//   });
+
+// database.ref().set({
+//   name: 'Ruth Newman',
+//   age: 30,
+//   stressLevel: 9,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'London',
+//     country: 'United Kingdom'
+//   }
+// }).then(() => {
+//   console.log('Data saved to the database');
+// }).catch((error) => {
+//   console.log('Failed to save correctly: ', error);
+// });
 
 // database.ref('attributes').set(null);
 
-database.ref().update({
-  stressLevel: 6,
-  'location/city': 'Manchester',
-  'job/company': 'The Co-op'
-});
+// database.ref().update({
+//   stressLevel: 6,
+//   'location/city': 'Manchester',
+//   'job/company': 'The Co-op'
+// });
 
 // database.ref('openToNewOpportunities').remove()
 //   .then(() => {
