af5cbe8d98efaf73ac7ed6ce965b2e4b8b9f03d5
Ruth Newman <ruthnewman@gmail.com>
2019-08-11 19:54:47 +0100

    added login button and set up auth with google auth provider

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,13 +5,15 @@ import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import NotFound from '../components/NotFound';
 import Header from '../components/Header';
+import LoginPage from '../components/LoginPage';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={Dashboard} exact={true}/>
+        <Route path="/" component={LoginPage} exact={true}/>
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/create" component={AddExpense}/>
         <Route path="/edit/:id" component={EditExpense} />
         <Route component={NotFound} />
