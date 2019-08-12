00a48e3dceab2ef03b8705b33ee5b940fa084f74
Ruth Newman <ruthnewman@gmail.com>
2019-08-12 18:15:44 +0100

    styling the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Log in</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Lean Life</h1>
+      <p>Manage your expenses and streamline your life</p>
+      <button onClick={startLogin}>Log in</button>
+    </div>
   </div>
 );
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   startLogin: () => dispatch(startLogin())
 });
 
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(LoginPage);
