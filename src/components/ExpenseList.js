a01f14ab865bf10551e7e126ea53aaf748a424dd
Ruth Newman <ruthnewman@gmail.com>
2019-08-14 01:48:18 +0100

    adding more styles to expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import ExpenseListItem from './ExpenseListItem';
-import selectExpenses from '../selectors/expenses';
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseListItem from "./ExpenseListItem";
+import selectExpenses from "../selectors/expenses";
 
-export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses yet!</p>
-      ) : (
-        props.expenses.map((expense) => {
-          return <ExpenseListItem key={expense.id} {...expense} />
-        })
-      )
-    }
+export const ExpenseList = props => (
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    {props.expenses.length === 0 ? (
+      <p>No expenses yet!</p>
+    ) : (
+      props.expenses.map(expense => {
+        return <ExpenseListItem key={expense.id} {...expense} />;
+      })
+    )}
   </div>
 );
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     expenses: selectExpenses(state.expenses, state.filters)
   };
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
-import numeral from 'numeral';
-import 'numeral/locales/en-gb';
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+import "numeral/locales/en-gb";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => {
-  numeral.locale('en-gb');
-  return (<div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
+  numeral.locale("en-gb");
+  return (
+    <Link className="list-item" to={`/edit/${id}`}>
+      <div>
+        <h3>{description}</h3>
+        <span> {moment(createdAt).format("dddd Do MMMM YYYY")}</span>
+      </div>
+      <h3>{numeral(amount / 100).format("$#,##0.00")}</h3>
     </Link>
-    <p>
-      <span>{numeral(amount / 100).format('$#,##0.00')} </span>
-       -
-      <span> {moment(createdAt).format("dddd Do MMMM YYYY")}</span>
-    </p>
-  </div>
-);
-}
+  );
+};
 
 export default ExpenseListItem;
